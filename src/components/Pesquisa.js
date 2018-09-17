36250b13ae273f76c776fbebe314b71bf7f827a4
nazio11 <nazio.silva@gmail.com>
2018-09-17 12:55:04 -0300

    Exindo lista de clientes e usando o componente de ScrollView

--- src/components/Pesquisa.js
+++ src/components/Pesquisa.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 import React from "react";
 
-import { View, Text, TouchableOpacity, Alert } from "react-native";
+import { View, ScrollView, Text, TouchableOpacity } from "react-native";
 import { SearchBar } from "react-native-elements";
 import { ListItem } from "react-native-elements";
-import SideMenu from "react-native-side-menu";
 import Icon from "react-native-vector-icons/dist/FontAwesome";
 
 const URL_BUSCA_CLIENTE =
@@ -20,24 +19,31 @@ export default class MenuDrawer extends React.Component {
       page: 1,
       requesting: false,
       listaClientes: [],
-      nav:null,
+      nav: null,
     };
     this.pesquisar = this.pesquisar.bind(this);
   }
 
   componentWillMount() {
-
     const token = this.props.token;
     const cli_id = this.props.clienteId;
 
-    console.log("PESQUISA: " + token)
-    
+    console.log("PESQUISA: " + token);
+
     this.setState({
       token,
       cli_id,
     });
 
-    const BUSCA_CLIENTE = URL_BUSCA_CLIENTE + token + "&cliente=" + cli_id + "&page="+this.state.page+"&query=" + this.state.texto;
+    const BUSCA_CLIENTE =
+      URL_BUSCA_CLIENTE +
+      token +
+      "&cliente=" +
+      cli_id +
+      "&page=" +
+      this.state.page +
+      "&query=" +
+      this.state.texto;
 
     axios
       .get(BUSCA_CLIENTE)
@@ -50,7 +56,7 @@ export default class MenuDrawer extends React.Component {
 
   pesquisar(texto) {
     this.setState({ texto });
-    if(this.state.requesting == true){
+    if (this.state.requesting == true) {
       return true;
     }
 
@@ -58,23 +64,31 @@ export default class MenuDrawer extends React.Component {
 
     axios
       .get(
+        URL_BUSCA_CLIENTE +
+          this.state.token +
+          "&cliente=" +
+          this.state.cli_id +
+          "&page=" +
+          this.state.page +
+          "&query=" +
+          this.state.texto
+      )
+      .then(() => {
+        const BUSCA_CLIENTE =
           URL_BUSCA_CLIENTE +
           this.state.token +
           "&cliente=" +
           this.state.cli_id +
-          "&page="+this.state.page+"&query=" +
-          this.state.texto
-      )
-      .then(() => {
-        const BUSCA_CLIENTE = URL_BUSCA_CLIENTE + this.state.token + "&cliente=" + this.state.cli_id + "&page="+ this.state.page + "&query=" + this.state.texto
-            this.state.requesting = false;
-              axios.get(BUSCA_CLIENTE) 
-                .then(res => {
-                  const clientes = res.data.dados;
-                  this.setState({ listaClientes: clientes });
-                },
-              )
-              console.log("Encontrado")
+          "&page=" +
+          this.state.page +
+          "&query=" +
+          this.state.texto;
+        this.state.requesting = false;
+        axios.get(BUSCA_CLIENTE).then(res => {
+          const clientes = res.data.dados;
+          this.setState({ listaClientes: clientes });
+        });
+        console.log("Encontrado");
       })
       .catch(() => {
         this.state.requesting = false;
@@ -87,53 +101,56 @@ export default class MenuDrawer extends React.Component {
         this.state.token +
         "&cliente=" +
         this.state.cli_id +
-        "&page="+this.state.page+"&query=" +
+        "&page=" +
+        this.state.page +
+        "&query=" +
         this.state.texto
     );
-  } 
+  }
 
   render() {
-
     return (
       <View style={{ flex: 1, backgroundColor: "#fff" }}>
-        <SideMenu style={{ flex: 1, backgroundColor: "#fff" }}>
-          <SearchBar
-            value={this.state.texto}
-            showLoading
-            lightTheme
-            placeholder="Pesquisar pelo cliente"
-            onChangeText={this.pesquisar}
-          />
-          {
-            this.state.listaClientes.map(cliente => {
+        <SearchBar
+          value={this.state.texto}
+          //showLoading
+          lightTheme
+          placeholder="Pesquisar pelo cliente"
+          onChangeText={this.pesquisar}
+        />
+        <ScrollView>
+          {this.state.listaClientes.map(cliente => {
             //console.log("Cliente: " + cliente.nome.split())
             const proprietario = cliente.nome.split();
             const dados = cliente.nome.toLowerCase();
 
             if (
               cliente.nome.indexOf(this.state.texto) != -1 ||
-              this.state.texto == "" || 
+              this.state.texto == "" ||
               dados.indexOf(this.state.texto) != -1
             ) {
-             
               return (
-                
-                <ListItem
-                  key={cliente.id}
-                  style={{
-                    backgroundColor: "#fff",
-                    marginLeft: 10,
-                    flexDirection: "row",
-                  }}
-                  leftIcon={
+                <View key={cliente.id}>
+                  <ListItem
+                    //key={cliente.id}
+                    style={{
+                      backgroundColor: "#fff",
+                      marginLeft: 10,
+                      flexDirection: "row",
+                    }}
+                    leftIcon={
                       <TouchableOpacity
-                        style={{ flexDirection: "row", backgroundColor: "#fff" }}
-                        onPress={() => { 
-                          console.log( "CLIENTE: " + cliente.nome ), 
-                          console.log( "CLIENTE TK: " + this.state.token ), 
-                          
-                          this.props.nav.navigate("Maps", [cliente.id, this.state.token]) 
-
+                        style={{
+                          flexDirection: "row",
+                          backgroundColor: "#fff",
+                        }}
+                        onPress={() => {
+                          //console.log( "CLIENTE: " + cliente.nome ),
+                          //console.log( "CLIENTE TK: " + this.state.token ),
+                          this.props.nav.navigate("Maps", [
+                            cliente.id,
+                            this.state.token,
+                          ]);
                         }}
                       >
                         <Icon
@@ -142,19 +159,19 @@ export default class MenuDrawer extends React.Component {
                           style={{ color: "black" }}
                         />
                         <Text style={{ marginLeft: 10, color: "black" }}>
-                          { proprietario }
+                          {proprietario}
                         </Text>
-                      </TouchableOpacity> 
-                  }
-                />
+                      </TouchableOpacity>
+                    }
+                  />
+                </View>
               );
             } else {
               console.log("Cliente nao encontrado!");
             }
           })}
-        </SideMenu>
+        </ScrollView>
       </View>
     );
   }
 }
-
